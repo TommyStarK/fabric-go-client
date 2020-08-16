4f6ee0e01e65cb16b967e3dd1316633e2e86fdd6
Thomas Milox <thomas.milox@ibm.com>
2020-08-16 18:17:22 +0200

    implement client config

---
+++ config.go
@@ -0,0 +1,85 @@
+package fabclient
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Chaincode struct {
+	BuildDir string `json:"buildDir" yaml:"buildDir"`
+	Name     string `json:"name" yaml:"name"`
+	Path     string `json:"path" yaml:"path"`
+	Policy   string `json:"policy,omitempty" yaml:"policy,omitempty"`
+	Version  string `json:"version" yaml:"version"`
+}
+
+type Channel struct {
+	AnchorPeerConfigPath string `json:"anchorPeerConfigPath" yaml:"anchorPeerConfigPath"`
+	ConfigPath           string `json:"configPath" yaml:"configPath"`
+	Name                 string `json:"name" yaml:"name"`
+}
+
+type Identity struct {
+	Certificate string `json:"certificate" yaml:"certificate"`
+	PrivateKey  string `json:"privateKey" yaml:"privateKey"`
+	Username    string `json:"username" yaml:"username"`
+}
+
+type Identities struct {
+	Admin Identity   `json:"admin" yaml:"admin"`
+	Users []Identity `json:"users" yaml:"users"`
+}
+
+// type TargetPeers struct {
+// 	NetworkAdministration []string `json:"networkAdministration" yaml:"networkAdministration"`
+// 	Transactions          []string `json:"transactions" yaml:"transactions"`
+// }
+
+type Config struct {
+	Chaincodes   []Chaincode `json:"chaincodes" yaml:"chaincodes"`
+	Channels     []Channel   `json:"channels" yaml:"channels"`
+	Identities   Identities  `json:"identities" yaml:"identities"`
+	Organization string      `json:"organization" yaml:"organization"`
+	// TargetPeers   TargetPeers `json:"targetPeers,omitempty" yaml:"targetPeers,omitempty"`
+	SDKConfigPath string `json:"sdkConfigPath" yaml:"sdkConfigPath"`
+	Version       string `json:"version" yaml:"version"`
+}
+
+func NewConfigFromFile(configPath string) (*Config, error) {
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	fileAsBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		config    = &Config{}
+		extension = filepath.Ext(configPath)
+	)
+
+	switch extension {
+	case ".json":
+		if err := json.Unmarshal(fileAsBytes, config); err != nil {
+			return nil, err
+		}
+	case ".yml", ".yaml":
+		if err := yaml.Unmarshal(fileAsBytes, config); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.New("invalid client configuration file extension, supported: .json .yml .yaml")
+	}
+
+	return config, nil
+}
---
+++ config_test.go
@@ -0,0 +1,35 @@
+package fabclient
+
+import (
+	"testing"
+)
+
+func TestNewConfigFromFile(t *testing.T) {
+	if _, err := NewConfigFromFile("./testdata/client/client-config.json"); err != nil {
+		t.Logf("should have succeed: %s", err.Error())
+		t.Fail()
+	}
+
+	if _, err := NewConfigFromFile("./testdata/client/client-config.yaml"); err != nil {
+		t.Logf("should have succeed: %s", err.Error())
+		t.Fail()
+	}
+
+	if _, err := NewConfigFromFile(""); err == nil {
+		t.Log("should have failed, invalid path")
+		t.Fail()
+	}
+
+	if _, err := NewConfigFromFile("./go.mod"); err == nil {
+		t.Log("should have failed: path towards a not supported extension file")
+		t.Fail()
+	}
+
+	if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.yaml"); err == nil {
+		t.Fail()
+	}
+
+	if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.json"); err == nil {
+		t.Fail()
+	}
+}
