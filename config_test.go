43a44f591da6969de97d7f07240eee27fd3a2800
TommyStarK <thomasmilox@gmail.com>
2020-08-21 19:26:55 +0200

    use testing.Error/testing.Errorf instead of testing.Log+testing.Fail, add test for invalid client config

--- config_test.go
+++ config_test.go
@@ -6,30 +6,27 @@ import (
 
 func TestNewConfigFromFile(t *testing.T) {
 	if _, err := NewConfigFromFile("./testdata/client/client-config.json"); err != nil {
-		t.Logf("should have succeed: %s", err.Error())
-		t.Fail()
+		t.Errorf("should have succeed, error: %w", err)
 	}
 
 	if _, err := NewConfigFromFile("./testdata/client/client-config.yaml"); err != nil {
-		t.Logf("should have succeed: %s", err.Error())
-		t.Fail()
+		t.Errorf("should have succeed, error: %w", err)
 	}
 
 	if _, err := NewConfigFromFile(""); err == nil {
-		t.Log("should have failed, invalid path")
-		t.Fail()
+		t.Error("should have returned an error, invalid path")
 	}
 
 	if _, err := NewConfigFromFile("./go.mod"); err == nil {
-		t.Log("should have failed: path towards a not supported extension file")
+		t.Error("should have returned an error, path towards a not supported extension file")
+	}
+
+	if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.json"); err == nil {
 		t.Fail()
 	}
 
-	// if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.yaml"); err == nil {
-	// 	t.Fail()
-	// }
+	if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.yaml"); err == nil {
+		t.Fail()
+	}
 
-	// if _, err := NewConfigFromFile("./testdata/client/invalid-client-config.json"); err == nil {
-	// 	t.Fail()
-	// }
 }
