b2783185ce8e594f14e64847f3a0f117a0806cbb
TommyStarK <thomasmilox@gmail.com>
2020-09-06 21:00:09 +0200

    fix test script

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -12,11 +12,11 @@ do
   else
     docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME up -d orderer${i}.dummy.com;
   fi
-  sleep 10;
+  sleep 5;
 done
 
 docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME up -d peer0.org1.dummy.com peer0.org2.dummy.com;
-sleep 10;
+sleep 5;
 
 check=$(docker ps -aq -f status=exited  | wc -l);
 check=${check##*( )};
@@ -25,7 +25,7 @@ if [[ "$check" -ne 0 ]]; then
   exit 1;
 fi
 
-docker run --rm --network=${COMPOSE_PROJECT_NAME}_default -v `pwd`:/go/src/github.com/TommyStarK/fabric-go-client \
+docker run --rm --network=${COMPOSE_PROJECT_NAME}_test -v `pwd`:/go/src/github.com/TommyStarK/fabric-go-client \
   fabclient bash -c "go test -v -race -failfast --cover -covermode=atomic -coverprofile=coverage.out -mod=vendor";
 
 rc=$?;
