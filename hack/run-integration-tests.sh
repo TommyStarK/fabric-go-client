f8f78aea46b9e9bc2380fe1cd56d034aa69f628e
TommyStarK <thomasmilox@gmail.com>
2020-09-06 20:44:43 +0200

    clean test script, update submodule

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -18,21 +18,21 @@ done
 docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME up -d peer0.org1.dummy.com;
 sleep 5;
 
-check=$(docker ps -aq -f status=exited  | wc -l);
+check=$(docker ps -aq -f status=exited | wc -l);
 check=${check##*( )};
 
 if [[ "$check" -ne 0 ]]; then
   exit 1;
 fi
 
-docker run --rm --network=${COMPOSE_PROJECT_NAME}_default -v `pwd`:/go/src/github.com/TommyStarK/fabric-go-client \
+docker run --rm --network=${COMPOSE_PROJECT_NAME}_test -v `pwd`:/go/src/github.com/TommyStarK/fabric-go-client \
   fabclient bash -c "go test -v -race -failfast --cover -covermode=atomic -coverprofile=coverage.out -mod=vendor";
 
 rc=$?;
 
-XARGS="xargs -r"
+XARGS="xargs -r";
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  XARGS="xargs"
+  XARGS="xargs";
 fi
 
 docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME down;
