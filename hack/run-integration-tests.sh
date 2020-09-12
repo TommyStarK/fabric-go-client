ea265e00b4f18768e50568c0f26b70c0a21d7e19
TommyStarK <thomasmilox@gmail.com>
2020-09-13 00:46:35 +0200

    test script: specify fabclient image tag to delete

--- hack/run-integration-tests.sh
+++ hack/run-integration-tests.sh
@@ -6,7 +6,7 @@ export COMPOSE_PROJECT_NAME=fabclient_${BUILD_NUMBER}
 docker build . -t fabclient;
 
 docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME up -d orderer.dummy.com peer0.org1.dummy.com peer0.org2.dummy.com;
-sleep 10;
+sleep 60;
 
 check=$(docker ps -aq -f status=exited  | wc -l);
 check=${check##*( )};
@@ -28,7 +28,7 @@ fi
 docker-compose -f testdata/hyperledger-fabric-network/docker-compose.yaml --project-name $COMPOSE_PROJECT_NAME down;
 docker ps -a | grep "dev-peer0.\(org1\|org2\).dummy.com-fcacc_\(1.0\|2.0\)" | awk '{print $1}'| $XARGS docker rm -f;
 docker images | grep "dev-peer0.\(org1\|org2\).dummy.com-fcacc_\(1.0\|2.0\)" | awk '{print $3}'| $XARGS docker rmi -f;
-docker rmi -f fabclient;
+docker rmi -f fabclient:latest;
 docker volume prune -f;
 docker network prune -f;
 
