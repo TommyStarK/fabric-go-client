567231aedb83c09bcbb7e7616181d1999a13b8c7
TommyStarK <thomasmilox@gmail.com>
2020-09-13 14:38:48 +0200

    update example

--- example_test.go
+++ example_test.go
@@ -2,6 +2,7 @@ package fabclient
 
 import (
 	"log"
+	"time"
 )
 
 func Example() {
@@ -10,7 +11,10 @@ func Example() {
 		log.Fatal(err)
 	}
 
+	defer client.Close()
+
 	channel := client.Config().Channels[0]
+	chaincode := client.Config().Chaincodes[0]
 
 	if err := client.SaveChannel(channel.Name, channel.ConfigPath); err != nil {
 		log.Fatal(err)
@@ -19,4 +23,57 @@ func Example() {
 	if err := client.JoinChannel(channel.Name); err != nil {
 		log.Fatal(err)
 	}
+
+	chaincodePackageID, err := client.LifecycleInstallChaincode(chaincode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.LifecycleApproveChaincode(channel.Name, chaincodePackageID, chaincode); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.LifecycleCommitChaincode(channel.Name, chaincode); err != nil {
+		log.Fatal(err)
+	}
+
+	// Init chaincode
+	req := &ChaincodeRequest{
+		ChaincodeID: chaincode.Name,
+		Function:    "init",
+		IsInit:      true,
+	}
+
+	if _, err := client.Invoke(req, WithOrdererResponseTimeout(2*time.Second)); err != nil {
+		log.Fatal(err)
+	}
+
+	// Invoke/Query chaincode using default API
+	storeRequest := &ChaincodeRequest{
+		ChaincodeID: chaincode.Name,
+		Function:    "Store",
+		Args:        []string{"asset-test", `{"content": "this is a content test"}`},
+	}
+
+	storeResult, err := client.Invoke(storeRequest, WithOrdererResponseTimeout(2*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("store txID: %s", storeResult.TransactionID)
+
+	queryRequest := &ChaincodeRequest{
+		ChaincodeID: chaincode.Name,
+		Function:    "Query",
+		Args:        []string{"asset-test"},
+	}
+
+	queryResult, err := client.Query(queryRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("query content: %s", string(queryResult.Payload))
+
+	// using Gateway
 }
